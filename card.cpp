0ec9276f490176215998ff47305406b493470c8e
Woaf <bfazekas@inf.elte.hu>
2020-05-02 12:12:11 +0200

    Initial commit

---
+++ card.cpp
@@ -0,0 +1,6 @@
+#include "card.h"
+
+Card::Card(QWidget *parent) : QMainWindow(parent)
+{
+
+}
---
+++ card.h
@@ -0,0 +1,17 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include <QMainWindow>
+#include <QWidget>
+
+class Card : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit Card(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // CARD_H
